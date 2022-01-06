dea9ef183cdd6254740aa9c48a8523aa32dc0f92
EllyHa <jwha94@gmail.com>
2022-01-06 09:47:14 +0900

    4.3 함수 컴포넌트로 구현해보기(♻️)
    4.3 함수 컴포넌트로 구현해보기(♻️)

--- src/EventPractice.jsx
+++ src/EventPractice.jsx
@@ -1,15 +1,27 @@
 import React, { Component, useState } from 'react';
 
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  })
+  const {username, message} = form;
+  const onChange = e => {
+    const nextForm = {
+      ...form, // 기존의 form 내용을 이 자리에 복사한 뒤
+      [e.target.name]: e.target.value // 원하는 값을 덮어씌우기
+    };
+    setForm(nextForm);
+  };
+
   const onClick = () => {
     alert(`${username}: ${message}`);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: ''
+    });
   };
+  
   const onKeyPress = e => {
     if(e.key === 'Enter'){
       onClick();
@@ -23,14 +35,14 @@ const EventPractice = () => {
           name="username"
           placeholder="사용자명"
           value={username}
-          onChange={onChangeUsername}
+          onChange={onChange}
         />
         <input
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={message}
-          onChange={onChangeMessage}
+          onChange={onChange}
           onKeyPress={onKeyPress}
         />
         <button onClick={onClick}>확인</button>
@@ -39,3 +51,9 @@ const EventPractice = () => {
 
 }
 export default EventPractice;
+
+// 이전 코드에서는 e.target.name을 활용하지 않고 onChange 관련 함수 두 개를 따로 만들어주었습니다.
+// 인풋이 두 개밖에 없다면 이런 코드도 나쁘지 않지만 더 많아질 것같으면 e.target.name을 활용하는 것이 더 좋을 수 있습니다.
+// 이번에는 useSate를 통해 사용하는 상태에 문자열이 아닌 객체를 넣어보겠습니다.
+
+// e.target.name 값을 활용하려면, 위와 같이 userSate를 쓸 때 인풋 값들이 들어있는 form객체를 사용해주면 됩니다.
