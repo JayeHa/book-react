8df3dbb86071221a498eab1333b47392ff6aa89d
EllyHa <jwha94@gmail.com>
2022-01-17 18:40:55 +0900

    ✔️ 5.3 컴포넌트에 ref 달기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Component } from 'react';
+import ScrollBox from './5-ref/scrollBox';
 import ValidationSample from './5-ref/validationSample';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <ValidationSample />;
+    return (
+      <div>
+        <ScrollBox />
+      </div>
+    );
   }
 }
 
