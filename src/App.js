46bc3ce9cc5b448ab36d64b00c9e26e27030c5d6
정원 <66169832+JayeHa@users.noreply.github.com>
2022-01-19 20:46:45 +0900

    📌 6. 컴포넌트 반복

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import { Component } from 'react';
-import ScrollBox from './5-ref/scrollBox';
+import IterationSample from './6-component-repeat/iterationSample';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
