3c34d37c0c2a440dcc77942dc1dbb65c4d07a685
정원 <66169832+JayeHa@users.noreply.github.com>
2022-01-19 21:31:30 +0900

    6.4.2 데이터 추가기능 구현하기

--- src/6-component-repeat/iterationSample.jsx
+++ src/6-component-repeat/iterationSample.jsx
@@ -10,8 +10,25 @@ const IterationSample = () => {
   const [inputText, setInputText] = useState('');
   const [nextId, setNextId] = useState(5); // 새로 항목을 추가할 때 사용할 id
 
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, //nextId값을 id로 설정하고
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId값에 1을 더해준다.
+    setNames(nextNames); // names값을 업데이트한다.
+    setInputText(''); // inputText를 비운다.
+  };
+
   const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
-  return <ul>{nameList}</ul>;
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
