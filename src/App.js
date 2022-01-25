5ee877239b1f80f50d79efa4b08bcd9db0f39f83
정원 <66169832+JayeHa@users.noreply.github.com>
2022-01-25 23:34:48 +0900

    8.1.1 useState를 여러 번 사용하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Counter from './8-hooks/Counter';
+import Info from './8-hooks/info';
 
 const App = () => {
-  return <Counter />;
+  return <Info />;
 };
 
 export default App;
