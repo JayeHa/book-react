bae1a1c520aae0f3ecba51f00830135eb89cd45c
정원 <66169832+JayeHa@users.noreply.github.com>
2022-01-26 22:32:06 +0900

    8.2.3 뒷정리하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './8-hooks/Counter';
 import Info from './8-hooks/info';
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;
