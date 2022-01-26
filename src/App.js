3595ceb81712d75a3fb1491d839289cb9f9a71d6
정원 <66169832+JayeHa@users.noreply.github.com>
2022-01-26 23:29:54 +0900

    ✔️ 8.3 useReducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Counter from './8-hooks/Counter';
-import Info from './8-hooks/info';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Counter />;
 };
 
 export default App;
