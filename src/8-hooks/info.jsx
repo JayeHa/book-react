3e0f4905c4ce4f3e8e16b47555475f9dde53fe3d
정원 <66169832+JayeHa@users.noreply.github.com>
2022-01-26 22:02:27 +0900

    8.2.1 마운트될 때만 실행하고 싶을 때

--- src/8-hooks/info.jsx
+++ src/8-hooks/info.jsx
@@ -4,9 +4,9 @@ const Info = () => {
   const [name, setName] = useState('');
   const [nickName, setNickname] = useState('');
   useEffect(() => {
-    console.log('렌더링이 완료되었습니다!');
+    console.log('마운트될 때만 실행됩니다.');
     console.log({ name, nickName });
-  });
+  }, []);
 
   const onChangeName = (e) => {
     setName(e.target.value);
