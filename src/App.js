20fd540a2d061cfc73993583b9fdcf70973b6328
정원 <66169832+JayeHa@users.noreply.github.com>
2022-02-04 01:48:37 +0900

    10.2.3 TodoListItem과 TodoList만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoInsert from './10-todo-web-app/components/TodoInsert';
+import TodoList from './10-todo-web-app/components/TodoList';
 import TodoTemplate from './10-todo-web-app/components/TodoTemplate';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
