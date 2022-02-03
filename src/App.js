48558c66d184fdbc42080e922ee93484777a24e6
정원 <66169832+JayeHa@users.noreply.github.com>
2022-02-04 00:44:56 +0900

    10.2.2 TodoInsert 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import TodoInsert from './10-todo-web-app/components/TodoInsert';
 import TodoTemplate from './10-todo-web-app/components/TodoTemplate';
 
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
 };
 
 export default App;
