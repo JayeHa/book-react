f9c44fd43eba73cf22b03698a16fae17c7d5cf76
EllyHa <jwha94@gmail.com>
2022-02-21 12:33:23 +0900

    13.2.5 Link 컴포넌트를 사용하여 다른 주소로 이동하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { Route } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import About from './13-spa/About';
 import Home from './13-spa/Home';
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
