e2d1ef2645e824728d687b6cf6f2601c75eda491
EllyHa <jwha94@gmail.com>
2022-02-21 14:31:56 +0900

    ✔️ 13.4 URL파라미터와 쿼리 > 13.4.1 URL 파라미터

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Link, Route } from 'react-router-dom';
 import About from './13-spa/About';
 import Home from './13-spa/Home';
+import Profile from './13-spa/Profile';
 
 const App = () => {
   return (
@@ -12,10 +13,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
