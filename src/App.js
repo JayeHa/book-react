a439475cde10928346bca34645d5960f24f03b7c
EllyHa <jwha94@gmail.com>
2022-02-21 15:53:53 +0900

    ✔️ 13.5 서브 라우트

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Link, Route } from 'react-router-dom';
 import About from './13-spa/About';
 import HistorySample from './13-spa/HistorySample';
 import Home from './13-spa/Home';
-import Profile from './13-spa/Profile';
+import Profiles from './13-spa/Profiles';
 
 const App = () => {
   return (
@@ -15,10 +15,7 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
         <li>
           <Link to="/history">History 예제</Link>
@@ -27,7 +24,7 @@ const App = () => {
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
       <Route path="/history" component={HistorySample} />
     </div>
   );
