c7f26373cc1806783926e72099e6660915cbc58e
EllyHa <jwha94@gmail.com>
2022-02-22 14:41:58 +0900

    ✔️ 14.6 카테고리 기능 구현하기 > 14.6.1. 카테고리 선택 UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import Categories from './14-news-viwer/Categories';
 import NewsList from './14-news-viwer/NewsList';
 
 const App = () => {
-  return <NewsList />;
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
 };
 
 export default App;
