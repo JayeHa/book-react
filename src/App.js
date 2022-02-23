e37e1786a921aa996fb6b7297b716a90c8c3f197
EllyHa <jwha94@gmail.com>
2022-02-23 10:19:58 +0900

    15.2.3 Provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import ColorBox from './15-context-api/components/ColorBox';
+import ColorContext from './15-context-api/contexts/color';
 
 const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: 'red' }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 };
 
