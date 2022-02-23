2c04e3f3e52551d095a36cd3e1c231e6244ad361
EllyHa <jwha94@gmail.com>
2022-02-23 17:50:10 +0900

    15.3.3 색상 선택 컴포넌트 만들기 > UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import ColorBox from './15-context-api/components/ColorBox';
 import { ColorProvider } from './15-context-api/contexts/color';
+import SelectColors from './15-context-api/components/SelectColors';
 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
