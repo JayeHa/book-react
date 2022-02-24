932f6bad66aba8bcbb8373ae59697f83221a7fc1
EllyHa <jwha94@gmail.com>
2022-02-24 18:22:25 +0900

    📌 16. 리덕스 라이브러리 이해하기

---
+++ src/16-vanilla-redux/index.js
@@ -0,0 +1,82 @@
+const { createStore } = require('redux');
+
+//📌 DOM 레퍼런스 만들기
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+//📌 액션 타입과 액션 생성 함수 정의
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+//📌 초깃값 설정
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+//📌 리듀서 함수 정의
+// state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  // action.type에 따라 다른 작업을 처리함
+
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 불변성 유지를 해주어야 합니다.
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+//📌 스토어 만들기
+const store = createStore(reducer);
+
+//📌 render함수 만들기
+const render = () => {
+  const state = store.getState(); // 현재 상태를 불러옵니다.
+  // 토글 처리
+  if (state.toggle) {
+    divToggle.classList.add('active');
+  } else {
+    divToggle.classList.remove('active');
+  }
+  // 카운터 처리
+  counter.innerText = state.counter;
+};
+
+render();
+
+//📌 구독하기
+store.subscribe(render);
+
+//📌 액션 발생시키기
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
