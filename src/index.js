72bdbc376b9b3368181bbd8b257c974477216fbe
EllyHa <jwha94@gmail.com>
2022-02-25 11:49:10 +0900

    17.4.2 Provider 컴포넌트를 사용하여 프로젝트에 리덕스 적용하기 / 17.4.3 Redux DevTools의 설치 및 적용

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import rootReducer from './17-react-redux-tutorial/modules';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
