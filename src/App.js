fe8801580cbd7604a824036285f2f0abc160409b
EllyHa <jwha94@gmail.com>
2022-02-25 13:34:05 +0900

    17.5.2 TodosContainer 만들기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Todos from './17-react-redux-tutorial/components/Todos';
 import CounterContainer from './17-react-redux-tutorial/containers/CounterContainer';
+import TodosContainer from './17-react-redux-tutorial/containers/TodosContainer';
 
 const App = () => {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 };
