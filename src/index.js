b6fc0d22a950db5a0a24421e66bd9dd2c55f6cd1
EllyHa <jwha94@gmail.com>
2022-03-03 12:49:36 +0900

    18.2.2 redux-logger 사용하기

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './18-redux-middleware/modules';
-import loggerMiddleware from './18-redux-middleware/lib/loggerMiddleware';
+// import loggerMiddleware from './18-redux-middleware/lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
