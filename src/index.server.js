4359ead8f6d06c51446155ecb24c421dc938791d
EllyHa <jwha94@gmail.com>
2022-03-08 17:30:02 +0900

    20.4.6 redux-saga를 위한 서버 사이드 렌더링 작업

--- src/index.server.js
+++ src/index.server.js
@@ -7,8 +7,10 @@ import fs from 'fs';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware, { END } from 'redux-saga';
 import rootReducer from './20-serverside-rendering/modules';
 import PreloadContext from './20-serverside-rendering/lib/PreloadContext';
+import { rootSaga } from './20-serverside-rendering/modules/index';
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -55,7 +57,14 @@ const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해줍니다.
 
   const context = {};
-  const store = createStore(rootReducer, applyMiddleware(thunk));
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, sagaMiddleware),
+  );
+
+  const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
 
   const preloadContext = {
     done: false,
@@ -72,7 +81,9 @@ const serverRender = async (req, res, next) => {
   );
 
   ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링합니다.
+  store.dispatch(END); // redux-saga의 END 액션을 발생시키면 액션을 모니터링하는 사가들이 모두 종료됩니다.
   try {
+    await sagaPromise; // 기존에 진행 중이던 사가들이 모두 끝날 때까지 기다립니다.
     await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
   } catch (e) {
     return res.status(500);
