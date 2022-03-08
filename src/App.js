613707437cc324d2327ffc44eb8ab33e16597cfc
EllyHa <jwha94@gmail.com>
2022-03-08 10:13:37 +0900

    20.4.2 Users, UsersContainer 컴포넌트 준비하기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route } from 'react-router-dom';
 import Menu from './20-serverside-rendering/components/Menu';
 import BluePage from './20-serverside-rendering/pages/BluePage';
 import RedPage from './20-serverside-rendering/pages/RedPage';
+import UsersPage from './20-serverside-rendering/pages/UsersPage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <hr />
       <Route path="/red" component={RedPage} />
       <Route path="/blue" component={BluePage} />
+      <Route path="/users" component={UsersPage} />
     </div>
   );
 };
