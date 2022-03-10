882c1e26f33642cf317431d82fc862518e0f63f5
EllyHa <jwha94@gmail.com>
2022-03-10 17:46:11 +0900

    24.2.4.5 auth 리덕스 모듈에서 API 사용하기

--- src/24-blog-frontend/lib/createRequestSaga.js
+++ src/24-blog-frontend/lib/createRequestSaga.js
@@ -1,5 +1,11 @@
 import { call, put } from 'redux-saga/effects';
-import { startloading, finishLoading } from '../modules/loading';
+import { startLoading, finishLoading } from '../modules/loading';
+
+export const createRequestActionTypes = (type) => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return [type, SUCCESS, FAILURE];
+};
 
 export default function createRequestSaga(type, request) {
   const SUCCESS = `${type}_SUCCESS`;
