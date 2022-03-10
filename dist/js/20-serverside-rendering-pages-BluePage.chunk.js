5be6339e0baaab26bfe0be28317b7e243d6feaae
EllyHa <jwha94@gmail.com>
2022-03-10 10:49:19 +0900

    d

---
+++ dist/js/20-serverside-rendering-pages-BluePage.chunk.js
@@ -0,0 +1,1 @@
+"use strict";exports.id=852,exports.ids=[852],exports.modules={394:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});var t=r(997);const u=function(){return(0,t.jsx)("div",{className:"Blue",children:"Blue"})},n=function(){return(0,t.jsx)(u,{})}}};
---
+++ dist/js/20-serverside-rendering-pages-RedPage.chunk.js
@@ -0,0 +1,1 @@
+"use strict";exports.id=889,exports.ids=[889],exports.modules={226:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});var t=r(997);const d=function(){return(0,t.jsx)("div",{className:"Red",children:"Red"})},n=function(){return(0,t.jsx)(d,{})}}};
